5b3e90d48a19092e2842a4f2390eada99c2d3593
benkyokurabu <benkyokurabu@gmail.com>
2026-03-11 16:04:59 +0900

    Auto Sync Update

--- run_update_journal_AUTO.cmd
+++ run_update_journal_AUTO.cmd
@@ -5,14 +5,8 @@ echo ===================================================
 echo 授業日誌 全自動更新システム (起動中...)
 echo ===================================================
 
-REM 指定された絶対パスを使用してフォルダを設定します
-set "JOURNAL_DIR=C:\Users\kudok\OneDrive\●勉強クラブ共有\09　授業日誌"
-set "BACKUP_DIR=C:\Users\kudok\OneDrive\●勉強クラブ共有\授業日誌_バックアップ"
-
-echo フォルダ確認中: %JOURNAL_DIR%
-
-REM Pythonを実行してJSON作成・GitHub送信まで一気に行います
-python update_journal.py --journal-dir "%JOURNAL_DIR%" --backup-dir "%BACKUP_DIR%" --mode auto --do-backup
+REM フォルダのチェックはPython側で行うため、ここでは直接実行します
+python update_journal.py --mode auto --do-backup
 
 echo.
 echo ---------------------------------------------------
